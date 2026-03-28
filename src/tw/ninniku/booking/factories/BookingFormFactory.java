ef7f0f44360ad784d03bfc2d4f537cf8245b24e6
Ray Lee <ray.asia@gmail.com>
2026-03-28 17:17:49 +0800

    Update BookingFormFactory: reference BookingForm; keep BookingTimeline alias for existing AD_Form records

--- src/tw/ninniku/booking/factories/BookingFormFactory.java
+++ src/tw/ninniku/booking/factories/BookingFormFactory.java
@@ -2,18 +2,19 @@ package tw.ninniku.booking.factories;
 
 import org.adempiere.webui.factory.IFormFactory;
 import org.adempiere.webui.panel.ADForm;
-
-import tw.ninniku.booking.form.BookingTimeline;
+import tw.ninniku.booking.form.BookingForm;
 
 public class BookingFormFactory implements IFormFactory {
 
-	@Override
-	public ADForm newFormInstance(String formName) {
-		if(formName.contains("BookingTimeline"))
-		{
-			return new BookingTimeline().getForm();
-		}
-		return null;
-	}
-
+    @Override
+    public ADForm newFormInstance(String formName) {
+        if (formName != null && formName.contains("BookingForm")) {
+            return new BookingForm().getForm();
+        }
+        // Backwards compatibility: old AD_Form records may still have "BookingTimeline"
+        if (formName != null && formName.contains("BookingTimeline")) {
+            return new BookingForm().getForm();
+        }
+        return null;
+    }
 }
